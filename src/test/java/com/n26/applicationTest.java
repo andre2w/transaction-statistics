5f0a5b8760dc7cfc6faf218b0436a0d1ce8a17b8
Andre Torres <andre.torres@codurance.com>
2019-04-19 11:10:34 +0100

    Add unit test folder and maven plugin

---
+++ src/test/java/com/n26/applicationTest.java
@@ -0,0 +1,15 @@
+package com.n26;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class applicationTest {
+
+
+    @Test
+    public void name() {
+        assertTrue(false);
+    }
+}
