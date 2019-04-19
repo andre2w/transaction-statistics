055bb05a7182b287bd7ae3e10b20f38ae21ca0dc
Andre Torres <andre.torres@codurance.com>
2019-04-19 21:17:07 +0100

    Add test for TransactionAggregator retrieving statistics for multiple seconds

--- src/test/java/com/n26/transaction/TransactionAggregatorShould.java
+++ src/test/java/com/n26/transaction/TransactionAggregatorShould.java
@@ -1,6 +1,7 @@
 package com.n26.transaction;
 
 import com.n26.infrastructure.Clock;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.math.BigDecimal;
@@ -16,20 +17,38 @@ public class TransactionAggregatorShould {
     private static final ZonedDateTime NOW = ZonedDateTime.now(ZoneId.of("UTC"));
     private static final Transaction FIRST_TRANSACTION = new Transaction(new BigDecimal("100.00"), NOW);
     private static final Transaction SECOND_TRANSACTION = new Transaction(new BigDecimal("50.00"), NOW);
+    private Clock clock;
+    private TransactionAggregator transactionAggregator;
+    private static final TransactionStatistics TRANSACTION_STATISTICS =
+        new TransactionStatistics(new BigDecimal("150.00"), new BigDecimal("75.00"), new BigDecimal("100.00"), new BigDecimal("50.00"), 2);
+
+    @Before
+    public void setUp() throws Exception {
+        clock = mock(Clock.class);
+        transactionAggregator = new TransactionAggregator(clock);
+    }
 
     @Test
     public void aggregate_transaction_by_second() {
-        Clock clock = mock(Clock.class);
-        TransactionAggregator transactionAggregator = new TransactionAggregator(clock);
         given(clock.now()).willReturn(NOW);
         transactionAggregator.add(FIRST_TRANSACTION);
         transactionAggregator.add(SECOND_TRANSACTION);
 
         TransactionStatistics result = transactionAggregator.statisticsOfLast(1);
 
-        TransactionStatistics transactionStatistics =
-                new TransactionStatistics(new BigDecimal("150.00"), new BigDecimal("75.00"), new BigDecimal("100.00"), new BigDecimal("50.00"), 2);
-        assertEquals(transactionStatistics, result);
+        assertEquals(TRANSACTION_STATISTICS, result);
+    }
+
+    @Test
+    public void aggregate_transactions_from_multiple_seconds() {
+        given(clock.now()).willReturn(NOW);
+        Transaction transactionTwoSecondsAgo = new Transaction(new BigDecimal("50.00"), NOW.minusSeconds(2));
+        transactionAggregator.add(FIRST_TRANSACTION);
+        transactionAggregator.add(transactionTwoSecondsAgo);
+
+        TransactionStatistics result = transactionAggregator.statisticsOfLast(5);
+
+        assertEquals(TRANSACTION_STATISTICS, result);
     }
 
 }
