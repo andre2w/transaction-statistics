63b5e55f9c1930b8a782a9b298c593fca66b067d
Andre Torres <andre.torres@codurance.com>
2019-04-19 23:28:09 +0100

    Make TransactionStatistics serialize to string instead of number
      * Change how minimum value of transaction is decided.

--- src/main/java/com/n26/transaction/TransactionStatistics.java
+++ src/main/java/com/n26/transaction/TransactionStatistics.java
@@ -1,5 +1,8 @@
 package com.n26.transaction;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+
 import java.math.BigDecimal;
 import java.util.Objects;
 
@@ -7,14 +10,22 @@ import static java.math.BigDecimal.ROUND_HALF_UP;
 import static java.math.BigDecimal.valueOf;
 
 public class TransactionStatistics {
+    @JsonSerialize(using = ToStringSerializer.class)
     private BigDecimal sum;
+
+    @JsonSerialize(using = ToStringSerializer.class)
     private BigDecimal avg;
+
+    @JsonSerialize(using = ToStringSerializer.class)
     private BigDecimal max;
+
+    @JsonSerialize(using = ToStringSerializer.class)
     private BigDecimal min;
+
     private int count;
 
     static TransactionStatistics empty() {
-        return new TransactionStatistics(BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.valueOf(Double.MIN_VALUE), BigDecimal.valueOf(Double.MAX_VALUE), 0);
+        return new TransactionStatistics(BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO, 0);
     }
 
     public TransactionStatistics() {}
@@ -47,6 +58,42 @@ public class TransactionStatistics {
         return count;
     }
 
+    TransactionStatistics add(Transaction transaction) {
+        BigDecimal newSum = sum.add(transaction.amount());
+        BigDecimal newCount = new BigDecimal(count + 1);
+
+        return new TransactionStatistics(
+                newSum,
+                newSum.divide(newCount, BigDecimal.ROUND_HALF_UP),
+                max.max(transaction.amount()),
+                min(transaction),
+                newCount.intValue()
+        );
+    }
+
+    private BigDecimal min(Transaction transaction) {
+        if (count == 0) {
+            return transaction.amount();
+        }
+
+        return min.min(transaction.amount());
+    }
+
+    void merge(TransactionStatistics transactionStatistics) {
+        sum = sum.add(transactionStatistics.sum);
+        max = max.max(transactionStatistics.max);
+        min = minTransactionStatisticAmount(transactionStatistics);
+        count = count + transactionStatistics.count;
+        avg = sum.divide(valueOf(count), BigDecimal.ROUND_HALF_UP);
+    }
+
+    private BigDecimal minTransactionStatisticAmount(TransactionStatistics transactionStatistics) {
+        if (count == 0) {
+            return transactionStatistics.min;
+        }
+        return min.min(transactionStatistics.min);
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -62,26 +109,6 @@ public class TransactionStatistics {
     @Override
     public int hashCode() {
         return Objects.hash(sum, avg, max, min, count);
-    }
-
-    TransactionStatistics add(Transaction transaction) {
-        BigDecimal newSum = sum.add(transaction.amount());
-        BigDecimal newCount = new BigDecimal(count + 1);
-        return new TransactionStatistics(
-                newSum,
-                newSum.divide(newCount, BigDecimal.ROUND_HALF_UP),
-                max.max(transaction.amount()),
-                min.min(transaction.amount()),
-                newCount.intValue()
-        );
-    }
-
-    void merge(TransactionStatistics transactionStatistics) {
-        sum = sum.add(transactionStatistics.sum);
-        max = max.max(transactionStatistics.max);
-        min = min.min(transactionStatistics.min);
-        count = count + transactionStatistics.count;
-        avg = sum.divide(valueOf(count), BigDecimal.ROUND_HALF_UP);
     }
 
     @Override
